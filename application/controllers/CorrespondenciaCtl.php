7f7dd1c3311c6cfa24a02fffafe0d0452364dbbc
hadamo <hadamosieg@gmail.com>
2020-04-02 18:50:42 -0300

    Controller para correspondencias, métodos get implementados

---
+++ application/controllers/CorrespondenciaCtl.php
@@ -0,0 +1,121 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+require_once APPPATH . 'libraries/API_Controller.php';
+
+class CorrespondenciaCtl extends API_Controller {
+
+    /**
+     * @api {get} correspondencias/:codCompCurric Requisitar a correspondência de uma componente curricular
+     * @apiName get
+     * @apiGroup Correspondencia
+     *
+     * @apiParam {Number} codCompCurric Codigo unico de componente curricular.
+     *
+     * @apiSuccess {String} nomeDisc Nome da disciplina que a componente integraliza no projeto pedagógico de curso.
+     * @apiSuccess {Number} codCompCurric Código da componente curricular.
+     * @apiSuccess {String} codDisc Código da disciplina.
+     * @apiSuccess {String} nomeDiscCorresp Nome da disciplina correspondente que a componente correspondente integraliza no projeto pedagógico de curso.
+     * @apiSuccess {Number} codCompCorresp Código da componente curricular correspondente.
+     * @apiSuccess {String} codDiscCorresp Código da disciplina correspondente.
+     * @apiSuccess {Number} percentual Percentual de correspondencia entre a componente e sua componente correspondente.
+     */
+    public function get($codCompCurric)
+	{
+        header("Access-Control-Allow-Origin: *");
+        $this->_apiConfig(array(
+                'methods' => array('GET'), 
+            ));
+
+        $qb = $this->entity_manager->createQueryBuilder()
+                ->select('CONCAT( dep1.abreviatura,disc1.numDisciplina) as codDisc, disc1.nome as NomeDisc, cc1.codCompCurric as codCompCurric',
+                'CONCAT( dep2.abreviatura,disc2.numDisciplina) as codDiscCorresp, disc2.nome as NomeDiscCorresp, cc2.codCompCurric as codCompCorresp',
+                ' cor.percentual ')
+                ->from('Entities\Correspondencia','cor')
+                ->innerJoin('cor.componenteCurricular','cc1')
+                ->innerJoin('cc1.disciplina','disc1')
+                ->innerJoin('disc1.departamento','dep1')
+                ->innerJoin('cor.componenteCurricularCorresp','cc2')
+                ->innerJoin('cc2.disciplina','disc2')
+                ->innerJoin('disc2.departamento','dep2')
+                ->where('cc1.codCompCurric =' . $codCompCurric . 'OR cc2.codCompCurric =' . $codCompCurric)
+                ->getQuery();
+                
+        $correspondencia = $qb->getResult();    
+     
+        
+        if(empty($correspondencia))
+        {
+            $this->api_return(
+                array(
+                    'status' => false,
+                    'message' =>  'Correspondencia não encontrada!'
+                ),404
+            );
+            
+        }else{
+            $this->api_return(
+                array(
+                    'status' => true,
+                    'result' =>  $correspondencia
+                ),200
+            );
+        }
+    }
+
+    /**
+     * @api {get} correspondencias/ Listar todas as correspondências de componentes curriculares.
+     * @apiName getAll
+     * @apiGroup Correspondencia
+     *
+     *
+     * @apiSuccess {String} nomeDisc Nome da disciplina que a componente integraliza no projeto pedagógico de curso.
+     * @apiSuccess {Number} codCompCurric Código da componente curricular.
+     * @apiSuccess {String} codDisc Código da disciplina.
+     * @apiSuccess {String} nomeDiscCorresp Nome da disciplina correspondente que a componente correspondente integraliza no projeto pedagógico de curso.
+     * @apiSuccess {Number} codCompCorresp Código da componente curricular correspondente.
+     * @apiSuccess {String} codDiscCorresp Código da disciplina correspondente.
+     * @apiSuccess {Number} percentual Percentual de correspondencia entre a componente e sua componente correspondente.
+     */
+    public function getAll()
+	{
+        header("Access-Control-Allow-Origin: *");
+        $this->_apiConfig(array(
+                'methods' => array('GET'), 
+            ));
+
+        $qb = $this->entity_manager->createQueryBuilder()
+                ->select('CONCAT( dep1.abreviatura,disc1.numDisciplina) as codDisc, disc1.nome as NomeDisc, cc1.codCompCurric as codCompCurric',
+                'CONCAT( dep2.abreviatura,disc2.numDisciplina) as codDiscCorresp, disc2.nome as NomeDiscCorresp, cc2.codCompCurric as codCompCorresp',
+                ' cor.percentual ')
+                ->from('Entities\Correspondencia','cor')
+                ->innerJoin('cor.componenteCurricular','cc1')
+                ->innerJoin('cc1.disciplina','disc1')
+                ->innerJoin('disc1.departamento','dep1')
+                ->innerJoin('cor.componenteCurricularCorresp','cc2')
+                ->innerJoin('cc2.disciplina','disc2')
+                ->innerJoin('disc2.departamento','dep2')
+                ->getQuery();
+                
+        $correspondencia = $qb->getResult();    
+     
+        
+        if(empty($correspondencia))
+        {
+            $this->api_return(
+                array(
+                    'status' => false,
+                    'message' =>  'Correspondencia não encontrada!'
+                ),404
+            );
+            
+        }else{
+            $this->api_return(
+                array(
+                    'status' => true,
+                    'result' =>  $correspondencia
+                ),200
+            );
+        }
+    }
+}
