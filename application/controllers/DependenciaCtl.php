f594baabccd0ba0e27836174864eef9983b524ae
Guilherme Gonçalves dos Santos <guilhermegufes@gmail.com>
2020-04-07 19:46:20 -0300

    Update DependenciaCtl.php
    Alteração do metódos para o repositório e inserceção da documentação da API .

--- application/controllers/DependenciaCtl.php
+++ application/controllers/DependenciaCtl.php
@@ -1,31 +1,7 @@
 
 <?php defined('BASEPATH') OR exit('No direct script access allowed');
 
-/**
- * @api {get} dependencias/:codCompCurric/:codPreReq Solicitar depêndencias entre componentes curriculares.
- *
- * @apiName getByid
- * @apiGroup Dependencia
- *
- * @apiSuccess {Number} codCompCurric Código identificador de uma componente curricular.
- * @apiSuccess {String} Curso Nome do curso que a componente curricular pertence.
- * @apiSuccess {String} Nome Nome da uma componente curricular.
- * @apiSuccess {Number} codCompCurric Código identificador de uma componente curricular.
- * @apiSuccess {String} Nome Nome do pré-requisito da componente curricular.
- * @apiSuccess {Number} codPreRequisito Código identificador de uma componente curricular que é pré-requisito.
- * @apiExample {curl} Exemplo:
- *      curl -i http://dev.api.ppcchoice.ufes.br/dependencias/6/1
- * @apiSuccessExample {JSON} Success-Response:
- * HHTP/1.1 200 OK
- * 
- * {
- *     "nome": "Ciência da Computação",
- *     "codCompCurric": 6,
- *     "nome": "Cálculo II",
- *     "codPreRequisito": 1
- *     "nome": "Cálculo II",
- * }
-*/
+
 
 require_once APPPATH . 'libraries/API_Controller.php';
 
@@ -36,6 +12,32 @@ class DependenciaCtl extends API_Controller
     public function __construct() {
         parent::__construct();
     }
+
+    /**
+    * @api {get} dependencias/ Solicitar todas depêndencias existentes entre componentes curriculares.
+    *
+    * @apiName getAll
+    * @apiGroup Dependência
+    *
+    * @apiSuccess {String} Curso Nome do curso que a componente curricular pertence.
+    * @apiSuccess {Number} codCompCurric Código identificador de uma componente curricular.
+    * @apiSuccess {String} nomeCompCurric Nome da uma componente curricular.
+    * @apiSuccess {Number} codPreRequisito Código identificador de uma componente curricular que é pré-requisito.
+    * @apiSuccess {String} nomePreReq Nome do pré-requisito da componente curricular.
+    * 
+    * @apiExample {curl} Exemplo:
+    *      curl -i http://dev.api.ppcchoice.ufes.br/dependencias/6/1
+    * @apiSuccessExample {JSON} Success-Response:
+    *   HTTP/1.1 200 OK
+    *   {
+    *     "Curso": "Ciência da Computação",
+    *     "codCompCurric": 6,
+    *     "nomeCompCurric": "Cálculo II",
+    *     "codPreRequisito": 1
+    *     "nomePreReq": "Cálculo II",
+    *   }
+    */
+
     public function getAll()
     {
         header("Access-Control-Allow-Origin: *");
@@ -46,19 +48,7 @@ class DependenciaCtl extends API_Controller
 
         ));
         
-        $qb = $this->entity_manager->createQueryBuilder()
-        ->select('curso.nome as Curso, d.codCompCurric , disc.nome as nomeCompCurric, d.codPreRequisito, dp.nome as nomePreRequisito')
-        ->from('Entities\Dependencia', 'd')
-        ->innerJoin('d.componenteCurricular', 'cc')
-        ->innerJoin('cc.disciplina', 'disc')
-        ->innerJoin('d.preRequisito', 'pr')
-        ->innerJoin('pr.disciplina', 'dp ') 
-        ->innerJoin('cc.ppc', 'ppc')
-        ->innerJoin('ppc.curso', 'curso') 
-        ->getQuery();
-        
-        $result = $qb->getResult();
-        
+        $result = $this->entity_manager->getRepository('Entities\Dependencia')->findAll();
 
         if(!empty($result)){
             
@@ -77,11 +67,43 @@ class DependenciaCtl extends API_Controller
                     "message" => 'Depêndencia não encontrada!',
                 ),
             404);
-        }
-        
-        
+        }   
     }
-    public function getById($codCompCurric, $codPreRequisito)
+
+    /**
+    * @api {get} dependencias/:codCompCurric/:codPreReq Solicitar depêndencias entre componentes curriculares.
+    *
+    * @apiName getById
+    * @apiGroup Dependência
+    *
+    * @apiSuccess {String} Curso Nome do curso que a componente curricular pertence.
+    * @apiSuccess {Number} codCompCurric Código identificador de uma componente curricular.
+    * @apiSuccess {String} nomeCompCurric Nome da uma componente curricular.
+    * @apiSuccess {Number} codPreRequisito Código identificador de uma componente curricular que é pré-requisito.
+    * @apiSuccess {String} nomePreReq Nome do pré-requisito da componente curricular.
+    * 
+    * @apiExample {curl} Exemplo:
+    *      curl -i http://dev.api.ppcchoice.ufes.br/dependencias/6/1
+    * @apiSuccessExample {JSON} Success-Response:
+    *   HTTP/1.1 200 OK
+    *   {
+    *     "Curso": "Ciência da Computação",
+    *     "codCompCurric": 6,
+    *     "nomeCompCurric": "Cálculo II",
+    *     "codPreRequisito": 1
+    *     "nomePreReq": "Cálculo II",
+    *   }
+    *
+    * @api {get} http://dev.api.ppcchoice.ufes.br/dependencias/:codCompCurric/:codPreRequisito
+    * @apiErrorExample {json} Error-Response:
+    *    HTTP/1.1 404 Not Found
+    *    {
+    *      status": false,
+    *      "message": "Dependência não encontrado!"
+    *    }
+    */
+
+    public function getById($codCompCurric, $codPreReq)
     {
         
         header("Access-Control-Allow-Origin: *");
@@ -92,20 +114,8 @@ class DependenciaCtl extends API_Controller
 
         ));
         
-        $qb = $this->entity_manager->createQueryBuilder()
-        ->select('curso.nome as Curso, d.codCompCurric , disc.nome AS nomeCompCurric    , d.codPreRequisito, dp.nome as nomePreReq')
-        ->from('Entities\Dependencia', 'd')
-        ->innerJoin('d.componenteCurricular', 'cc')
-        ->innerJoin('cc.disciplina', 'disc')
-        ->innerJoin('d.preRequisito', 'pr')
-        ->innerJoin('pr.disciplina', 'dp ') 
-        ->innerJoin('cc.ppc', 'ppc')
-        ->innerJoin('ppc.curso', 'curso')
-        ->where('d.codCompCurric = ?1 AND d.codPreRequisito = ?2')
-        ->setParameters(array(1 => $codCompCurric , 2 =>$codPreRequisito))
-        ->getQuery();
-        
-        $result = $qb->getResult();
+        $result = $this->entity_manager->getRepository('Entities\Dependencia')->findById($codCompCurric, $codPreReq);
+
         
         if(!empty($result)){
             
@@ -128,6 +138,34 @@ class DependenciaCtl extends API_Controller
     }  
 
 
+    /**
+    * @api {get} projetos-pedagogicos-curso/:codPpc/dependencias Solicitar todas depêndencias entre componentes as curriculares de um Projeto Pedagógico de Curso.
+    *
+    * @apiName getByIdPpc
+    * @apiGroup Dependência
+    *
+    * @apiSuccess {Number} codCompCurric Código identificador de uma componente curricular.
+    * @apiSuccess {Number} codPreRequisito Código identificador de uma componente curricular que é pré-requisito.
+    *
+    * @apiExample {curl} Exemplo:
+    *      curl -i http://dev.api.ppcchoice.ufes.br/projetos-pedagogicos-curso/1/dependencias
+    * @apiSuccessExample {JSON} Success-Response:
+    * HTTP/1.1 200 OK
+    * 
+    * {
+    *     "codCompCurric": 6,
+    *     "codPreRequisito": 1
+    * }
+    *
+    * @api {get} http://dev.api.ppcchoice.ufes.br/dependencias/:codPpc
+    * @apiErrorExample {json} Error-Response:
+    *     HTTP/1.1 404 Not Found
+    *     {
+    *       status": false,
+    *       "message": "Dependência não encontrado!"
+    *     }
+    */
+    
     public function getByIdPpc($codPpc)
     {
         
@@ -138,16 +176,9 @@ class DependenciaCtl extends API_Controller
             'methods' => array('GET'), 
 
         ));
-        
-        $qb = $this->entity_manager->createQueryBuilder()
-        ->select('d.codCompCurric, d.codPreRequisito')
-        ->from('Entities\Dependencia', 'd')
-        ->innerJoin('d.componenteCurricular', 'cc')
-        ->where('cc.ppc = ?1')
-        ->setParameter(1,$codPpc)
-        ->getQuery();
-        
-        $result = $qb->getResult();
+    
+        $result = $this->entity_manager->getRepository('Entities\Dependencia')->findByIdPpc($codPpc);
+
         
         if(!empty($result)){
             
@@ -163,7 +194,7 @@ class DependenciaCtl extends API_Controller
             $this->api_return(
                 array(
                     'status' => false,
-                    "message" => 'Depêndencia não encontrada!',
+                    "message" => 'Dependencia não encontrada!',
                 ),
             404);
         }
