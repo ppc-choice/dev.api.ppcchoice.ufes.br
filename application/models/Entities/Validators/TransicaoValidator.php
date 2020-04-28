fc6496463c1ace36d44fcf06c6ca0ecf2c8a2822
hadamo <hadamosieg@gmail.com>
2020-04-28 16:48:19 -0300

    Correção de validators

--- application/models/Entities/Validators/TransicaoValidator.php
+++ application/models/Entities/Validators/TransicaoValidator.php
@@ -3,18 +3,19 @@ namespace Entities\Validators;
 
 use Symfony\Component\Validator\ExecutionContext;
 use Entities\Transicao;
-use Entities\ProjetoPedagogicoCurso;
 class TransicaoValidator
 {
     public static function ppcIgual(Transicao $transicao, ExecutionContext $context)
     {
-        if(!is_null($transicao))
+        $ppcAtual = $transicao->getPpcAtual();
+        $ppcAlvo = $transicao->getPpcAlvo();
+        if(!is_null( $ppcAtual) && !is_null( $ppcAlvo))
         {
-            $ppcAtual = $transicao->getPpcAtual()->getCodPpc();
-            $ppcAlvo  = $transicao->getPpcAlvo()->getCodPpc();
-            if ( $ppcAtual === $ppcAlvo ){
+            $codPpcAtual = $ppcAtual->getCodPpc();
+            $codPpcAlvo  = $ppcAlvo->getCodPpc();
+            if ( $codPpcAtual === $codPpcAlvo ){
                 $context->addViolationAt(
-                    'Transicao',
+                    'ppcAtual',
                     'Auto-transição não é permitida.',
                     array(),
                     null
