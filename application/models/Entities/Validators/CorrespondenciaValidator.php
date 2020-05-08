9c7d67e43db9a2cb3c96c896050b6917ba0fd6f9
hadamo <hadamosieg@gmail.com>
2020-05-07 23:51:51 -0300

    Add Violation transicao inexistente para correspondencia

--- application/models/Entities/Validators/CorrespondenciaValidator.php
+++ application/models/Entities/Validators/CorrespondenciaValidator.php
@@ -3,7 +3,8 @@ namespace Entities\Validators;
 
 use Symfony\Component\Validator\ExecutionContext;
 use Entities\Correspondencia;
-
+use Entities\Transicao;
+use Doctrine;
 class CorrespondenciaValidator
 {
     public static function ppcIgual(Correspondencia $corresp, ExecutionContext $context)
@@ -21,6 +22,33 @@ class CorrespondenciaValidator
                     array(),
                     null
                 );
+            }
+        }
+    }
+
+    public static function transicaoInexistente(Correspondencia $corresp, ExecutionContext $context)
+    {
+        $compCurric  = $corresp->getComponenteCurricular();
+        $compCorresp = $corresp->getComponenteCurricularCorresp();
+        if(!is_null($compCurric) && !is_null($compCorresp))
+        {
+            $ppc1 = $compCurric->getPpc();
+            $ppc2 = $compCorresp->getPpc();
+            if(!is_null($ppc1) && !is_null($ppc2))
+            {
+                $doctrine = new \Doctrine;
+                $em = $doctrine->getEntitymanager();
+                $transicao = $em->find('Entities\Transicao',
+                array('ppcAtual' => $ppc1->getCodPpc(), 'ppcAlvo' => $ppc2->getCodPpc()));
+                if(is_null($transicao))
+                {
+                    $context->addViolationAt(
+                        'transicao',
+                        'Os Componentes curriculares devem ser de ppcs com transição mapeada entre si.',
+                        array(),
+                        null
+                    );
+                }
             }
         }
     }
