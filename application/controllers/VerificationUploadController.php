c108dca41ce336f58ea8b1b889f6712fedc4540f
elyabe <elyabe.as@gmail.com>
2020-10-16 16:31:37 -0300

    Add Verification Controller

--- application/controllers/VerificationUploadController.php
+++ application/controllers/VerificationUploadController.php
@@ -1,11 +1,12 @@
-<?php defined('BASEPATH') OR exit('No direct script access allowed');
+<?php defined('BASEPATH') or exit('No direct script access allowed');
 
 require_once APPPATH . 'libraries/APIController.php';
 
 
-class VerificationUploadController extends APIController 
+class VerificationUploadController extends APIController
 {
-    public function __construct() {
+    public function __construct()
+    {
         parent::__construct();
     }
 
@@ -29,61 +30,113 @@ class VerificationUploadController extends APIController
 
         $this->_apiConfig(array(
             'methods' => array('POST'),
-            )
-        );
+        ));
 
         $payload = $this->getBodyRequest();
- 
+
+        if (
+            !array_key_exists('codUnidadeEnsino', $payload)  ||
+            !array_key_exists('codPpcAtual', $payload) ||
+            !array_key_exists('codPpcAlvo', $payload) ||
+            !array_key_exists('conjuntoSelecao', $payload)
+        ) {
+            $this->apiReturn(
+                array(
+                    'message' => array('Campo obrigatório não informado ou contém valor inválido')
+                ),
+                self::HTTP_BAD_REQUEST
+            );
+        }
+
         $ue = $this->entityManager->find('Entities\UnidadeEnsino', $payload['codUnidadeEnsino']);
-        
-        if ( is_null($ue) ) {
-			$this->apiReturn(array('message' => array('Unidade de Ensino Inválida')), self::HTTP_BAD_REQUEST);
-		}
 
-        $ppcAtual = $this->entityManager->find('Entities\ProjetoPedagogicoCurso',$payload['codPpcAtual']);
+        if (is_null($ue)) {
+            $this->apiReturn(
+                array(
+                    'message' => array('Unidade de Ensino Inválida')
+                ),
+                self::HTTP_BAD_REQUEST
+            );
+        }
 
-        if ( is_null($ppcAtual) ) {
-			$this->apiReturn(array('message' => array('PPC Atual Inválido')), self::HTTP_BAD_REQUEST);
-		}
+        $ppcAtual = $this->entityManager->find('Entities\ProjetoPedagogicoCurso', $payload['codPpcAtual']);
 
-        $ppcAlvo = $this->entityManager->find('Entities\ProjetoPedagogicoCurso',$payload['codPpcAlvo']);
+        if (is_null($ppcAtual)) {
+            $this->apiReturn(
+                array(
+                    'message' => array('PPC Atual Inválido')
+                ),
+                self::HTTP_BAD_REQUEST
+            );
+        }
 
-        if ( is_null($ppcAlvo) ) {
-			$this->apiReturn(array('message' => array('PPC Alvo Inválido')), self::HTTP_BAD_REQUEST);
-		}
+        $ppcAlvo = $this->entityManager->find('Entities\ProjetoPedagogicoCurso', $payload['codPpcAlvo']);
 
-        $transicao = $this->entityManager->find('Entities\Transicao', 
-            array('ppcAtual' => $payload['codPpcAtual'], 'ppcAlvo' => $payload['codPpcAlvo']));
+        if (is_null($ppcAlvo)) {
+            $this->apiReturn(
+                array(
+                    'message' => array('PPC Alvo Inválido')
+                ),
+                self::HTTP_BAD_REQUEST
+            );
+        }
 
-            if ( is_null($transicao) ) {
-                $this->apiReturn(array('message' => array('Transição de PPC Inválido')), self::HTTP_BAD_REQUEST);
-            }
+        $transicao = $this->entityManager->find(
+            'Entities\Transicao',
+            array('ppcAtual' => $payload['codPpcAtual'], 'ppcAlvo' => $payload['codPpcAlvo'])
+        );
 
-        foreach ($payload['conjuntoSelecao'] as $componente){
+        if (is_null($transicao)) {
+            $this->apiReturn(
+                array(
+                    'message' => array('Transição de PPC Inválido')
+                ),
+                self::HTTP_BAD_REQUEST
+            );
+        }
+
+        foreach ($payload['conjuntoSelecao'] as $componente) {
             $verificationComponente = $this->entityManager->find('Entities\ComponenteCurricular', $componente);
 
-            if ( is_null($verificationComponente) ) {
-                $this->apiReturn(array('message' => array('Componente Inválida')), self::HTTP_BAD_REQUEST);
+            if (is_null($verificationComponente)) {
+                $this->apiReturn(
+                    array(
+                        'message' => array('Componente Inválida')
+                    ),
+                    self::HTTP_BAD_REQUEST
+                );
             }
 
-            $compPertencePpcAtual = $this->entityManager->getRepository('Entities\ComponenteCurricular')->findOneBy(array('ppc'=> $ppcAtual, 'codCompCurric' => $componente));
-            
-            if ( is_null($compPertencePpcAtual) ) {
-                $this->apiReturn(array('message' => array('A componente não pertence ao PPC Atual')), self::HTTP_BAD_REQUEST);
+            $compPertencePpcAtual = $this->entityManager->getRepository('Entities\ComponenteCurricular')
+                ->findOneBy(array('ppc' => $ppcAtual, 'codCompCurric' => $componente));
+
+            if (is_null($compPertencePpcAtual)) {
+                $this->apiReturn(
+                    array(
+                        'message' => array('A componente não pertence ao PPC Atual')
+                    ),
+                    self::HTTP_BAD_REQUEST
+                );
             }
         }
 
         // Verificar se o ppc atual pertence a unidade de ensino
         $codUes = $ppcAtual->getCurso()->getUnidadeEnsino()->getCodUnidadeEnsino();
 
-        if ( $ue->getCodUnidadeEnsino() != $codUes) {
-            $this->apiReturn(array('message' => array('O PPC Atual não pertence a Unidade de Ensino')), self::HTTP_BAD_REQUEST);
+        if ($ue->getCodUnidadeEnsino() != $codUes) {
+            $this->apiReturn(
+                array(
+                    'message' => array('O PPC Atual não pertence a Unidade de Ensino')
+                ),
+                self::HTTP_BAD_REQUEST
+            );
         }
-        
-        $this->apiReturn(array(
-            'message' => array('Verificação feita com sucesso!'),
-            ), self::HTTP_OK
+
+        $this->apiReturn(
+            array(
+                'message' => array('Verificação feita com sucesso!'),
+            ),
+            self::HTTP_OK
         );
     }
-
 }
