601570960570c962842b9a2d89df20d4ea6c99ab
elyabe <elyabe.as@gmail.com>
2020-10-23 16:24:32 -0300

    Fix transition query

--- application/models/Entities/Repositories/TransicaoRepository.php
+++ application/models/Entities/Repositories/TransicaoRepository.php
@@ -1,8 +1,8 @@
 <?php
+
 namespace Entities\Repositories;
 
 use Doctrine\ORM\EntityRepository;
-use Doctrine\ORM\Mapping as ORM;
 
 //Autor: Hádamo
 class TransicaoRepository extends EntityRepository
@@ -10,14 +10,14 @@ class TransicaoRepository extends EntityRepository
     public function findAll()
     {
         return $this->_em->createQueryBuilder()
-            ->select("CONCAT(CONCAT(cursoAtual.nome,' ('), CONCAT(pAtual.anoAprovacao,')')) as ppcAtual, pAtual.codPpc as codPpcAtual,
-                    CONCAT(CONCAT(cursoAlvo.nome,' ('), CONCAT(pAlvo.anoAprovacao,')')) as ppcAlvo, pAlvo.codPpc as codPpcAlvo")
-            ->from('Entities\Transicao','t')
-            ->innerJoin('t.ppcAtual','pAtual')
-            ->innerJoin('t.ppcAlvo','pAlvo')
-            ->innerJoin('pAtual.curso','cursoAtual')
-            ->innerJoin('pAlvo.curso','cursoAlvo')
-            ->orderBy('pAtual.codPpc, pAlvo.codPpc','ASC')
+            ->select("cursoAtual.nome  as ppcAtual, pAtual.anoAprovacao as anoPpcAtual, pAtual.codPpc as codPpcAtual,
+                    cursoAlvo.nome as ppcAlvo, pAlvo.anoAprovacao as anoPpcAlvo , pAlvo.codPpc as codPpcAlvo")
+            ->from('Entities\Transicao', 't')
+            ->innerJoin('t.ppcAtual', 'pAtual')
+            ->innerJoin('t.ppcAlvo', 'pAlvo')
+            ->innerJoin('pAtual.curso', 'cursoAtual')
+            ->innerJoin('pAlvo.curso', 'cursoAlvo')
+            ->orderBy('pAtual.codPpc, pAlvo.codPpc', 'ASC')
             ->getQuery()
             ->getResult();
     }
@@ -25,14 +25,14 @@ class TransicaoRepository extends EntityRepository
     public function findByCodUnidadeEnsino($codUnidadeEnsino)
     {
         return $this->_em->createQueryBuilder()
-            ->select("CONCAT(CONCAT(c.nome,' ('), CONCAT(p.anoAprovacao,')')) as nomeCurso, p.codPpc")
-            ->from('Entities\Transicao','t')
-            ->innerJoin('t.ppcAtual','p')
-            ->innerJoin('p.curso','c')
-            ->innerJoin('c.unidadeEnsino','ues1')
-            ->where('ues1.codUnidadeEnsino = :codUe'  )
-            ->setParameter('codUe',$codUnidadeEnsino )
-            ->orderBy('ues1.codUnidadeEnsino,p.codPpc','ASC')
+            ->select("c.nome as nomeCurso, p.anoAprovacao, p.codPpc")
+            ->from('Entities\Transicao', 't')
+            ->innerJoin('t.ppcAtual', 'p')
+            ->innerJoin('p.curso', 'c')
+            ->innerJoin('c.unidadeEnsino', 'ues1')
+            ->where('ues1.codUnidadeEnsino = :codUe')
+            ->setParameter('codUe', $codUnidadeEnsino)
+            ->orderBy('ues1.codUnidadeEnsino,p.codPpc', 'ASC')
             ->getQuery()
             ->getResult();
     }
@@ -40,18 +40,17 @@ class TransicaoRepository extends EntityRepository
     public function findByCodPpc($codPpcAtual)
     {
         return $this->_em->createQueryBuilder()
-            ->select("CONCAT(CONCAT(cursoAtual.nome,' ('), CONCAT(pAtual.anoAprovacao,')')) as ppcAtual, pAtual.codPpc as codPpcAtual,
-                    CONCAT(CONCAT(cursoAlvo.nome,' ('), CONCAT(pAlvo.anoAprovacao,')')) as ppcAlvo, pAlvo.codPpc as codPpcAlvo")
-            ->from('Entities\Transicao','t')
-            ->innerJoin('t.ppcAtual','pAtual')
-            ->innerJoin('t.ppcAlvo','pAlvo')
-            ->innerJoin('pAtual.curso','cursoAtual')
-            ->innerJoin('pAlvo.curso','cursoAlvo')
+            ->select("cursoAtual.nome  as ppcAtual, pAtual.anoAprovacao as anoPpcAtual, pAtual.codPpc as codPpcAtual,
+                    cursoAlvo.nome as ppcAlvo, pAlvo.anoAprovacao as anoPpcAlvo , pAlvo.codPpc as codPpcAlvo")
+            ->from('Entities\Transicao', 't')
+            ->innerJoin('t.ppcAtual', 'pAtual')
+            ->innerJoin('t.ppcAlvo', 'pAlvo')
+            ->innerJoin('pAtual.curso', 'cursoAtual')
+            ->innerJoin('pAlvo.curso', 'cursoAlvo')
             ->where('pAtual.codPpc = :codPpcAtual')
-            ->setParameter('codPpcAtual',$codPpcAtual )
-            ->orderBy('pAtual.codPpc, pAlvo.codPpc','ASC')
+            ->setParameter('codPpcAtual', $codPpcAtual)
+            ->orderBy('pAtual.codPpc, pAlvo.codPpc', 'ASC')
             ->getQuery()
             ->getResult();
-
     }
 }
